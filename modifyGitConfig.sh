76c5438fe80e732b09533fa148e06f9262f55453
Mrzzchao <mrzzhichao@gmail.com>
2021-03-31 00:34:06 +0800

    feat: update

---
+++ modifyGitConfig.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# 设置项目邮箱.
+git config user.email "mrzzhichao@gmail.com"
+ 
+# 设置项目用户名
+git config user.name "Mrzzchao" 
+
+git remote add origin "https://github.com/Mrzzchao/learn-react-source.git"
